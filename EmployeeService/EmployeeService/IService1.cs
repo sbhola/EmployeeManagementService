f25372c1c370bc80413f36fd7256e6329d7742da
SIDDHARTH BHOLA <siddharthbhola123@yahoo.co.in>
2014-08-16 13:08:26 +0530

    Basic EmployeeManagementService structure added.

---
+++ EmployeeService/EmployeeService/IService1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EmployeeService
+{
+
+    [ServiceContract]
+    public interface ICreateOrModifyEmployee
+    {
+        [OperationContract]
+        void CreateEmployee(int id, string name,string remarks);
+
+        [OperationContract]
+        bool AddRemarks(int id, string remarks);
+    }
+
+    //[ServiceContract]
+    //public interface IRetrieveEmpDetails
+    //{
+
+    //}
+    
+    [DataContract]
+    public class Employee
+    {
+        [DataMember]
+        public int EmpID { get; set; }
+        [DataMember]
+        public string EmpName { get; set; }
+        [DataMember]
+        public Remarks Remark { get; set; }
+
+    }
+
+    public class Remarks
+    {
+        public string text { get; set; }
+        public DateTime remarkTimestamp { get; set; }
+    }
+
+}
+
---
+++ EmployeeService/EmployeeService/Service1.svc.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace EmployeeService
+{
+    public class EmployeeManagementService : ICreateOrModifyEmployee
+    {
+        List<Employee> Employees = new List<Employee>();
+
+        public void CreateEmployee(int id, string name, string remarks)
+        {
+            Employee emp = new Employee();
+            emp.EmpID = id;
+            emp.EmpName = name;
+
+            Remarks remark = new Remarks();
+            remark.text = remarks;
+            remark.remarkTimestamp = DateTime.Now;
+
+            emp.Remark = remark;
+
+            Employees.Add(emp);
+
+        }
+
+        public bool AddRemarks(int id, string remarks)
+        {
+            return false;
+        }
+    }
+}
